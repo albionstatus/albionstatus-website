8227731911864f4690b01ad25be952f58223a913
Alexander Lichter <manniL@gmx.net>
2017-08-09 16:34:38 +0200

    Add non-promise based handling for notifications
    Fix #8

--- src/js/shared/notification.service.js
+++ src/js/shared/notification.service.js
@@ -1,14 +1,30 @@
 'use strict'
 
 exports.authorize = function () {
-  return Notification.requestPermission()
-    .then(function (permission) {
-      if (permission === 'denied')
-        return
-      else if (permission === 'default')
-        return
-      // Do something with the granted permission, if needed.
-    })
+  try {
+    return Notification.requestPermission()
+      .then(function (permission) {
+        if (permission === 'denied')
+          return
+        else if (permission === 'default')
+          return
+        // Do something with the granted permission, if needed.
+      })
+  } catch (error) {
+    // Safari doesn't return a promise for requestPermissions and it
+    // throws a TypeError. It takes a callback as the first argument
+    // instead.
+    if (error instanceof TypeError) {
+      Notification.requestPermission((permission) => {
+        if (permission === 'denied')
+          return
+        else if (permission === 'default')
+          return
+      })
+    } else {
+      throw error
+    }
+  }
 }
 
 exports.show = function (title, options) {
