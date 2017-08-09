bb243da080a744bb38c8169d95b114e267368ee1
Alexander Lichter <manniL@gmx.net>
2017-08-09 16:43:40 +0200

    Ignore error type for safari
    Fix #8

--- src/js/shared/notification.service.js
+++ src/js/shared/notification.service.js
@@ -11,19 +11,13 @@ exports.authorize = function () {
         // Do something with the granted permission, if needed.
       })
   } catch (error) {
-    // Safari doesn't return a promise for requestPermissions and it
-    // throws a TypeError. It takes a callback as the first argument
-    // instead.
-    if (error instanceof TypeError) {
-      Notification.requestPermission((permission) => {
-        if (permission === 'denied')
-          return
-        else if (permission === 'default')
-          return
-      })
-    } else {
-      throw error
-    }
+    // Safari fix
+    Notification.requestPermission((permission) => {
+      if (permission === 'denied')
+        return
+      else if (permission === 'default')
+        return
+    })
   }
 }
 
