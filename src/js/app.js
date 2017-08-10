f8f368e22375643f2c4a3fd38902214c27afd1c2
Alexander Lichter <manniL@gmx.net>
2017-08-10 16:58:41 +0200

    Remove VueAdsense and fix container sizing

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueMoment from 'vue-moment'
 import moment from 'moment-timezone'
-import VueAdsense from 'vue-adsense'
 
 //Own components
 import ServerStatus from './components/ServerStatus.vue'
@@ -15,7 +14,6 @@ Vue.use(VueAxios, axios)
 Vue.use(VueMoment, {
   moment
 })
-Vue.component('adsense', VueAdsense)
 
 new Vue({
   components: {
