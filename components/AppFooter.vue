ebf4982b1649bda2502ebdaf4525aaad3fddc266
Alexander Lichter <github@lichter.io>
2020-05-01 12:43:10 +0200

    design: improve footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,16 +2,19 @@
   <footer class="bg-white">
     <div class="max-w-screen-xl mx-auto py-6 md:py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
       <div class="flex justify-center">
-        <p class="text-center text-base leading-6 text-gray-400 max-w-3xl md:max-w-none">
-          Powered by
-          <a
-            class="text-developmint-300 hover:text-developmint-500"
-            href="https://developmint.de?ref=albionstatus.com"
-            rel="noopener"
-            target="_blank"
-          >Developmint</a>
-          &copy; {{ $options.currentYearNumbers }} &mdash; All rights reserved. Albion Online is a registered
-          trademark of Sandbox Interactive GmbH in Germany, and/or other countries.
+        <p class="flex flex-col md:block text-base leading-6 text-gray-400 max-w-3xl md:max-w-xl xl:max-w-none">
+          <span>Powered by
+            <a
+              class="text-developmint-300 hover:text-developmint-500"
+              href="https://developmint.de?ref=albionstatus.com"
+              rel="noopener"
+              target="_blank"
+            >Developmint</a>
+            &copy; {{ $options.currentYearNumbers }}</span>
+          <span class="md:block xl:inline-block xl:ml-1">
+            All rights reserved. Albion Online is a registered
+            trademark of Sandbox Interactive GmbH in Germany, and/or other countries.
+          </span>
         </p>
       </div>
       <div class="flex justify-center items-center mt-4 text-sm md:mt-0">
