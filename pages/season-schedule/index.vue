aed1bff40a824950fcd4b0c4b7774ece2d9fd40e
Alexander Lichter <github@lichter.io>
2021-03-08 14:36:21 +0100

    chore: update schedule

--- pages/season-schedule/index.vue
+++ pages/season-schedule/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="prose">
-      <h1>Current Season - Season 11</h1>
-      Albion's Guild Season 11 began on <strong>Saturday, December 5 2020</strong> at 11:00 UTC and will end on
-      <strong>Monday, March 1 2021</strong> 09:59 UTC.<br>
+      <h1>Current Season - Season 12</h1>
+      Albion's Guild Season 12 begins on <strong>Saturday, March 13 2020</strong> at 11:00 UTC and will end on
+      <strong>Monday, June 7 2021</strong> 09:59 UTC.<br>
       <h2>Reset Days / Invasion Days</h2>
       <p>
         On so called <em>Invasion Days</em> &ndash; also referred to as Reset Days &ndash; claimed guild
@@ -15,14 +15,14 @@
         with a primetime between 22:00 and 5:00 UTC will become available at 01:00 UTC on the following day. The reset times create a balance
         between EU and NA core play times.
       </p>
-      <p>The reset days for seasons 11 were:</p>
+      <p>The reset days for seasons 12 are:</p>
       <ul>
-        <li><strong>December 5, 2020</strong></li>
-        <li><strong>January 2, 2021</strong></li>
-        <li><strong>January 30, 2021</strong></li>
+        <li><strong>March 13, 2021</strong></li>
+        <li><strong>April 10, 2021</strong></li>
+        <li><strong>May 8, 2021</strong></li>
       </ul>
       <h2>Time until end of the season</h2>
-      <EndOfSeasonTimer :number="11" end-date="2021-03-01" />
+      <EndOfSeasonTimer :number="12" end-date="2021-06-07" />
     </div>
   </div>
 </template>
@@ -30,7 +30,7 @@
 
 export default {
   head () {
-    const title = 'Season 11 Schedule'
+    const title = 'Season 12 Schedule'
     const metaDescription = 'Read more about the schedule of the current Albion Online season, reset days and territory claiming'
 
     return {
