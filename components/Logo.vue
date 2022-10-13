eb4b11f8cf5d7ae3bb5acadb46a0ad6063d56f19
Alexander Lichter <github@lichter.io>
2022-10-13 13:21:24 +0200

    perf: lazy-load logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -4,7 +4,7 @@
       <source type="image/avif" srcset="~/assets/logo_small_128.avif 2x, ~/assets/logo_small_64.avif 1x">
       <source type="image/webp" srcset="~/assets/logo_small_128.webp 2x, ~/assets/logo_small_64.webp 1x">
       <source type="image/png" srcset="~/assets/logo_small_128.png 2x, ~/assets/logo_small_64.png 1x">
-      <img src="~/assets/logo_small_128.png" alt="AlbionStatus Logo">
+      <img src="~/assets/logo_small_128.png" alt="AlbionStatus Logo" loading="lazy">
     </picture>
   </a>
 </template>
