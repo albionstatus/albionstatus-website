f74872cdc7660c1c5a9d54b1b0bb1781d5b8f92b
Alexander Lichter <github@lichter.io>
2022-10-13 02:53:54 +0200

    fix: srcset

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,9 +1,9 @@
 <template>
   <a href="/" @click.prevent="jump">
     <picture class="h-16 w-auto">
-      <source type="image/avif" srcset="~/assets/logo_small_128.avif 2x, 1x ~/assets/logo_small_64.avif">
-      <source type="image/webp" srcset="~/assets/logo_small_128.webp 2x, 1x ~/assets/logo_small_64.webp">
-      <source type="image/png" srcset="~/assets/logo_small_128.png 2x, 1x ~/assets/logo_small_64.png">
+      <source type="image/avif" srcset="~/assets/logo_small_128.avif 2x, ~/assets/logo_small_64.avif 1x">
+      <source type="image/webp" srcset="~/assets/logo_small_128.webp 2x, ~/assets/logo_small_64.webp 1x">
+      <source type="image/png" srcset="~/assets/logo_small_128.png 2x, ~/assets/logo_small_64.png 1x">
       <img src="~/assets/logo_small_128.png" alt="AlbionStatus Logo">
     </picture>
   </a>
