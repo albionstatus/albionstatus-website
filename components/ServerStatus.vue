83a7a5f3da67e5aa38c4053b0215c634aaa02ac4
Alexander Lichter <github@lichter.io>
2023-03-13 21:48:07 +0100

    feat: show both statuses at once

--- components/ServerStatus.vue
+++ components/ServerStatus.vue
@@ -1,60 +1,8 @@
 <script setup lang="ts">
-import { DateTime } from 'luxon'
-import type { DurationUnits } from 'luxon'
-import { DATE_OF_CREATION } from '@/shared/constants'
-import type { StatusApiResponse } from '~/types'
+import { DurationUnits, DateTime } from 'luxon';
 
-/*
- * Constants
- */
-const SERVER_STATUS_NOTIFICATION = {
-  TITLE: 'AlbionStatus - Server status changed',
-  BODY_PREFIX: 'The server is now',
-  TAG: 'server-status-notification',
-  TIMEOUT: 10000,
-  ICON: '',
-}
-
-const status = ref('???')
-const statusClassLookup: Record<string, string> = {
-  online: 'text-green-700',
-  offline: 'text-red-800',
-  default: 'text-yellow-400',
-}
-const statusClasses = computed(() => statusClassLookup[status.value] ?? statusClassLookup.default)
-
-const lastCheckedAt = ref<DateTime | null>(null)
-const formattedLastCheckedAt = computed(() => lastCheckedAt.value?.toFormat('HH:mm:ss') ?? '...')
-
-const message = ref('')
-const showMessage = computed(() => !['online', '???'].includes(status.value))
-
-const isFirstCheck = ref(true)
-watch(status, () => {
-  // Ignore change from "???" to the real status
-  if (isFirstCheck.value) {
-    isFirstCheck.value = false
-    return
-  }
-  displayServerStatusNotification()
-})
-
-const { isSupported, show, close } = useWebNotification({
-  title: SERVER_STATUS_NOTIFICATION.TITLE,
-  body: `${SERVER_STATUS_NOTIFICATION.BODY_PREFIX} ${status.value}!`,
-  tag: SERVER_STATUS_NOTIFICATION.TAG,
-  icon: SERVER_STATUS_NOTIFICATION.ICON,
-})
-
-function displayServerStatusNotification() {
-  if (!isSupported.value)
-    return
-
-  show()
-  setTimeout(() => {
-    close()
-  }, SERVER_STATUS_NOTIFICATION.TIMEOUT)
-}
+const { status: statusWest, statusClasses: statusClassesWest, message: messageWest, showMessage: showMessageWest, formattedLastCheckedAt } = useStatus('west')
+const { status: statusEast, statusClasses: statusClassesEast, message: messageEast, showMessage: showMessageEast } = useStatus('east')
 
 const units: DurationUnits = ['years', 'months', 'days', 'hours']
 const duration = DateTime.local().diff(DateTime.fromISO(DATE_OF_CREATION), units)
@@ -63,67 +11,56 @@ const operatingSince = units.map(u => duration.get(u) && `${Math.ceil(duration.g
   .join(', ')
   .replace(/,([^,]*)$/, ' and $1')
 
-const { data, refresh } = useFetch<StatusApiResponse>('/api/status/west/', { server: false })
-watch(data, (data) => {
-  if (!data)
-    return
-
-  setStatus(data)
-})
-
-useIntervalFn(() => {
-  refresh()
-}, 30 * 1000)
-
-function setStatus(newStatus: StatusApiResponse) {
-  // Track last status so we know when to inform the user of a status change
-  status.value = newStatus.type
-  lastCheckedAt.value = DateTime.fromISO(newStatus.created_at)
-  message.value = newStatus.message ?? ''
-}
 </script>
 
 <template>
   <div class="mt-4 lg:mt-16 pt-4 lg:pt-16 pb-4 lg:pb-12 bg-gray-100 rounded">
     <div class="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="max-w-4xl mx-auto">
-        <dl class="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-2">
+        <dl class="rounded-t-lg bg-white shadow-lg sm:grid sm:grid-cols-2">
           <div class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
             <dt id="item-1" class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-              Server status
+              Albion West Server status
             </dt>
-            <dd class="order-1 text-5xl leading-none font-extrabold" :class="statusClasses" aria-describedby="item-1">
-              {{ status }}
+            <dd class="order-1 text-5xl leading-none font-extrabold" :class="statusClassesWest" aria-describedby="item-1">
+              {{ statusWest }}
             </dd>
           </div>
+          <div class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
+            <dt id="item-2" class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
+              Albion East Server status
+            </dt>
+            <dd class="order-1 text-5xl leading-none font-extrabold" :class="statusClassesEast" aria-describedby="item-2">
+              {{ statusEast }}
+            </dd>
+          </div>
+        </dl>
+        <dl class="border-t rounded-b-lg bg-white shadow-lg sm:grid sm:grid-cols-1">
           <div
-            class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r"
-          >
+            class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
             <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
               Last checked at
             </dt>
-            <dd class="order-1 text-5xl leading-none font-extrabold text-indigo-600">
+            <dd class="order-1 text-5xl leading-none font-extrabold text-indigo-600" aria-describedby="item-2">
               {{ formattedLastCheckedAt }}
             </dd>
           </div>
         </dl>
-        <Transition
-          enter-active-class="transition-opacity ease-linear duration-300" enter-class="opacity-0"
+        <Transition enter-active-class="transition-opacity ease-linear duration-300" enter-class="opacity-0"
           enter-to-class="opacity-100" leave-active-class="transition-opacity ease-linear duration-300"
-          leave-class="opacity-100" leave-to-class="opacity-0"
-        >
-          <dl v-show="showMessage" class="mt-4 rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-1">
+          leave-class="opacity-100" leave-to-class="opacity-0">
+          <dl class="mt-4 rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-1">
             <div class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
               <dd class="text-3xl leading-none text-center text-gray-700">
-                {{ message }}
+                <p v-show="showMessageWest">Albion West: {{ messageWest }}<br></p>
+                <p v-show="showMessageEast">Albion East: {{ messageEast }}</p>
               </dd>
             </div>
           </dl>
         </Transition>
         <dl class="mt-4 rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-1">
           <div
-            class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r"
-          >
+            class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
             <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
               Operating since
             </dt>
---
+++ composables/useStatus.ts
@@ -0,0 +1,83 @@
+import { DateTime } from 'luxon'
+import type { DurationUnits } from 'luxon'
+import { DATE_OF_CREATION } from '@/shared/constants'
+import type { StatusApiResponse } from '~/types'
+
+const SERVER_STATUS_NOTIFICATION = {
+  TITLE: 'AlbionStatus - Server status changed',
+  BODY_PREFIX: 'The server is now',
+  TAG: 'server-status-notification',
+  TIMEOUT: 10000,
+  ICON: '',
+}
+
+const STATUS_CLASS_LOOKUP: Record<string, string> = {
+  online: 'text-green-700',
+  offline: 'text-red-800',
+  default: 'text-yellow-400',
+}
+
+export function useStatus (server: 'east' | 'west') {
+  const status = ref('???')
+  const isFirstCheck = ref(true)
+  const statusClasses = computed(() => STATUS_CLASS_LOOKUP[status.value] ?? STATUS_CLASS_LOOKUP.default)
+
+  const lastCheckedAt = ref<DateTime | null>(null)
+  const formattedLastCheckedAt = computed(() => lastCheckedAt.value?.toFormat('HH:mm:ss') ?? '...')
+
+  const message = ref('')
+  const showMessage = computed(() => !['online', '???'].includes(status.value))
+
+  watch(status, () => {
+    // Ignore change from "???" to the real status
+    if (isFirstCheck.value) {
+      isFirstCheck.value = false
+      return
+    }
+    displayServerStatusNotification()
+  })
+
+  const { isSupported, show, close } = useWebNotification({
+    title: SERVER_STATUS_NOTIFICATION.TITLE,
+    body: `${SERVER_STATUS_NOTIFICATION.BODY_PREFIX} ${status.value}!`,
+    tag: SERVER_STATUS_NOTIFICATION.TAG,
+    icon: SERVER_STATUS_NOTIFICATION.ICON,
+  })
+
+  function displayServerStatusNotification () {
+    if (!isSupported.value)
+      return
+
+    show()
+    setTimeout(() => {
+      close()
+    }, SERVER_STATUS_NOTIFICATION.TIMEOUT)
+  }
+
+  const { data, refresh: refresh } = useFetch<StatusApiResponse>(`/api/status/${server}`, { server: false, key: `status-call-${server}` })
+  watch(data, (data) => {
+    if (!data)
+      return
+
+    setStatus(data)
+  })
+
+  useIntervalFn(() => {
+    refresh()
+  }, 30 * 1000)
+
+  function setStatus (newStatus: StatusApiResponse) {
+    // Track last status so we know when to inform the user of a status change
+    status.value = newStatus.type
+    lastCheckedAt.value = DateTime.fromISO(newStatus.created_at)
+    message.value = newStatus.message ?? ''
+  }
+
+  return {
+    status: readonly(status),
+    statusClasses: readonly(statusClasses),
+    formattedLastCheckedAt: readonly(formattedLastCheckedAt),
+    message: readonly(message),
+    showMessage
+  }
+}
